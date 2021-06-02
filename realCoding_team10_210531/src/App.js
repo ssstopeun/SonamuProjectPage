f65d7afcaa0373e829ba49ae2af04dc8f385c81a
Ji Eun <jieun1416@gmail.com>
2021-06-02 15:12:41 +0900

    add advance47

--- realCoding_team10_210531/src/App.js
+++ realCoding_team10_210531/src/App.js
@@ -1,37 +1,20 @@
 import * as React from 'react';
 import 'react-native-gesture-handler';
 
-import { StatusBar } from 'expo-status-bar';
-import { View, StyleSheet } from 'react-native';
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import CityList from './screens/CityList';
-import WeatherDetailScreen from './screens/WeatherDetailScreen';
 
-const HomeScreen = ({ navigation }) => (
-    <View style={styles.container}>
-        <CityList navigation={navigation} />
-        <StatusBar style="auto" />
-    </View>
-    );
-
-const DetailScreen = ({ navigation, route }) => (
-    <View style={styles.container}>
-      <WeatherDetailScreen navigation={navigation} route={route} />
-      <StatusBar style="auto" />
-    </View>
-);
-
-const Stack = createStackNavigator();
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
+import{
+    HomeScreen,
+    DetailScreen,
+}from './screens';
 
 export default class App extends React.Component {
+    static Stack = createStackNavigator();
   render() {
+      const Stack = App.Stack;
+
     return (
       <NavigationContainer>
         <Stack.Navigator initialRouteName="Home">
