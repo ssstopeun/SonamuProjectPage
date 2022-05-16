a84b3db59a9f6d33284f816cc9db01c808eefa2c
정지은 <68415975+ssstopeun@users.noreply.github.com>
2022-05-16 17:45:22 +0900

    upload initial folder

---
+++ SonamuProject/src/main/java/com/example/SonamuProject/SonamuProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.SonamuProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonamuProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonamuProjectApplication.class, args);
+	}
+
+}
---
+++ SonamuProject/src/main/java/com/example/SonamuProject/SonamuProjectController/homeController.java
@@ -0,0 +1,12 @@
+package com.example.SonamuProject.SonamuProjectController;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class homeController {
+
+
+}
