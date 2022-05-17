399991be12bc7afedbdd03a04e7e78ef9fe681ad
Gmelon <gmelon24kr@gmail.com>
2022-05-17 21:44:16 +0900

    파일명 수정

--- src/main/java/com/example/SonamuProject/controller/HomeController.java
+++ src/main/java/com/example/SonamuProject/controller/HomeController.java
@@ -1,12 +1,11 @@
-package com.example.SonamuProject.SonamuProjectController;
+package com.example.SonamuProject.controller;
 
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
-public class homeController {
+public class HomeController {
 
     @GetMapping("/")
     public String home() {
